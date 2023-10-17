131faf9f8d237bf115109d8b83e12125f9d01347
Rensi <rensidudhat2411@gmail.com>
2023-10-17 15:16:25 +0530

    navbar & footer done

---
+++ r_footer/r_footer.js
@@ -0,0 +1,64 @@
+function footer(){
+    return` 
+    <div id="footer_container">
+        <div id="footer_box">
+            <div>
+                <h4>About Us</h4>
+                <p>Auto Parts Spares ensures the complete durability and compatibility of its products, it has an upper
+                    hand over the automobile marketing industry. We have successfully established our name in the car
+                    spare parts market with our loyalty and trustworthiness over customers.</p>
+            </div>
+            <div>
+                <h4>Get in Touch</h4>
+                <p>447 Broadway, F1 2,<br> New York, NY, 10013</p>
+                <p>info@convermax.com</p>
+                <p>800-451- 0972</p>
+            </div>
+            <div>
+                <h4>Quick Links</h4>
+                <p>Customer Service</p>
+                <p>Return Policy</p>
+                <p>Shipping & Delivery</p>
+                <p>Our Story</p>
+                <p>About Us</p>
+                <p>Blog</p>
+            </div>
+            <div>
+                <h4>Get in Touch</h4>
+                <input type="text" placeholder="Email Address">
+                <button>Subscribe</button>
+                <div>
+                    <span><i class="fa-brands fa-facebook-f"></i></span>
+                    <span><i class="fa-brands fa-twitter"></i></span>
+                    <span><i class="fa-brands fa-instagram"></i></span>
+                    <span><i class="fa-brands fa-linkedin-in"></i></span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- footer End -->
+
+    <div id="copyrights">
+        <div id="copyrights_box">
+            <div>
+                <div>
+                    <img src="https://i.postimg.cc/VLNrKz2t/l12.png" alt="logoAuto">
+                </div>
+            </div>
+            <div>
+                <p>copyrights 2023 <span id="yAUot"> AUTO PART</span></p>
+                <p>All rights reserved.</p>
+            </div>
+            <div>
+                <div>
+                    <img src="https://i.postimg.cc/52x1785s/allcards.png" alt="AllCards">
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+export {footer};
