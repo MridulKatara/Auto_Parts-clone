a75b1fb617dadd6185f79383c459f9ea2835ba4f
SursaChauhan <ss0622187@gmail.com>
2023-10-22 19:41:05 +0530

    almost done

--- productpage/product.js
+++ productpage/product.js
@@ -1,250 +1,18 @@
 
-
-// const productsContainer = document.getElementById("products_container");
-
-// // Updated JSON data to match your mock server response
-// const mockData = [
-//   {
-//     partId: "123",
-//     name: "Brake Pad",
-//     category: {
-//       categoryId: "1",
-//       name: "BATTERIES",
-//     },
-//     brand: {
-//       brandId: "100",
-//       name: "ACME",
-//     },
-//     price: 2699,
-//     quantity: 100,
-//     image:
-//       "https://media.gettyimages.com/id/95757579/photo/profile-and-side-profile-view-of-a-car-wheel-on-white.jpg?s=612x612&w=gi&k=20&c=awd-KrLiALOhoDkrJtFVdyuoDSpDzWHCCpw3vJ4mi50=",
-//   },
-//   {
-//     partId: "123",
-//     name: "Castrol",
-//     category: {
-//       categoryId: "2",
-//       name: "Engines",
-//     },
-//     brand: {
-//       brandId: "100",
-//       name: "ACME",
-//     },
-//     price: 2599,
-//     quantity: 100,
-//     image:
-//       "https://media.gettyimages.com/id/95757579/photo/profile-and-side-profile-view-of-a-car-wheel-on-white.jpg?s=612x612&w=gi&k=20&c=awd-KrLiALOhoDkrJtFVdyuoDSpDzWHCCpw3vJ4mi50=",
-//   },
-//   {
-//     partId: "123",
-//     name: "Engines",
-//     category: {
-//       categoryId: "3",
-//       name: "BATTERIES",
-//     },
-//     brand: {
-//       brandId: "100",
-//       name: "ACME",
-//     },
-//     price: 1399,
-//     quantity: 100,
-//     image:
-//       "https://www.autozone.com/cdn/images/B2C/US/media/content/FeatCat/evergreen-grid4-batteries-d.jpg",
-//   },
-//   {
-//     partId: "123",
-//     name: "Brake Pad",
-//     category: {
-//       categoryId: "1",
-//       name: "TYRES SET",
-//     },
-//     brand: {
-//       brandId: "100",
-//       name: "ACME",
-//     },
-//     price: 1299,
-//     quantity: 100,
-//     image:
-//       "https://www.autozone.com/cdn/images/B2C/US/media/content/FeatCat/evergreen-grid4-wipers-d.jpg",
-//   },
-//   {
-//     partId: "123",
-//     name: "Brake Pad",
-//     category: {
-//       categoryId: "1",
-//       name: "Engine oil",
-//     },
-//     brand: {
-//       brandId: "100",
-//       name: "ACME",
-//     },
-//     price: 25.99,
-//     quantity: 100,
-//     image:
-//       "https://media.gettyimages.com/id/95757579/photo/profile-and-side-profile-view-of-a-car-wheel-on-white.jpg?s=612x612&w=gi&k=20&c=awd-KrLiALOhoDkrJtFVdyuoDSpDzWHCCpw3vJ4mi50=",
-//   },
-// ];
-
-// // Flag to track whether sorting is applied
-// let isSortingApplied = false;
-
-// // Function to display the data
-// function displayData(data) {
-//   // Clear the existing products container
-//   productsContainer.innerHTML = '';
-
-//   data.forEach((product) => {
-//     const productContainer = document.createElement("div");
-//     productContainer.className = "product";
-
-//     const productImage = document.createElement("img");
-//     productImage.src = product.image;
-//     productImage.alt = product.name;
-//     productImage.classList.add("product_img_container");
-
-//     const productName = document.createElement("h2");
-//     productName.textContent = product.name;
-
-//     const productPrice = document.createElement("p");
-//     productPrice.textContent = `$${product.price.toFixed(2)}`;
-
-//     productContainer.appendChild(productImage);
-//     productContainer.appendChild(productName);
-//     productContainer.appendChild(productPrice);
-
-//     productsContainer.appendChild(productContainer);
-//   });
-// }
-
-// // Call the function to display data using the original mock data
-// displayData(mockData);
-
-// // Sorting
-// const sortingSelect = document.getElementById("sorting");
-
-// sortingSelect.addEventListener("change", () => {
-//   const selectedOption = sortingSelect.value;
-//   sortAndDisplay(selectedOption);
-// });
-
-// function sortAndDisplay(selectedOption) {
-//   // Sort the original data based on the selected option if sorting is not applied
-//   if (!isSortingApplied) {
-//     isSortingApplied = false;
-
-//     switch (selectedOption) {
-//       case "atoz":
-//         mockData.sort((a, b) => a.name.localeCompare(b.name));
-//         break;
-//       case "ztoa":
-//         mockData.sort((a, b) => b.name.localeCompare(a.name));
-//         break;
-//       case "lowtohigh":
-//         mockData.sort((a, b) => a.price - b.price);
-//         break;
-//       case "hightolow":
-//         mockData.sort((a, b) => b.price - a.price);
-//         break;
-//       default:
-//         // If no valid sorting option is selected, reset the flag
-//         isSortingApplied = false;
-//         break;
-//     }
-//   }
-
-//   // Display the sorted or unsorted data
-//   displayData(mockData);
-// }
-
-// // Call the initial display when the page loads
-// sortAndDisplay(sortingSelect.value);
-
-// // filter part 
-
-// // Filter data based on selected filters
-// function filterAndDisplay() {
-//     const selectedFilters = [];
-
-//     // Get selected product categories
-//     const checkboxes = document.querySelectorAll('#filters input[type="checkbox"]');
-//     checkboxes.forEach((checkbox) => {
-//         if (checkbox.checked) {
-//             selectedFilters.push(checkbox.value); // Use the "value" attribute for category names
-//         }
-//     });
-
-//     // Get selected price range
-//     const priceRangeFilter = document.getElementById('price_range_filter');
-//     const selectedPrice = priceRangeFilter.value;
-
-//     // Filter products based on selected filters
-//     const filteredData = mockData.filter((product) => {
-//         const categoryMatch = selectedFilters.length === 0 || selectedFilters.includes(product.category.name.toUpperCase());
-//         const priceMatch = product.price <= selectedPrice;
-//         return categoryMatch && priceMatch;
-//     });
-
-//     // Call the function to display the filtered data
-//     displayData(filteredData);
-// }
-
-// // Add event listeners for checkboxes and price range filter
-// const filterCheckboxes = document.querySelectorAll('#filters input[type="checkbox"]');
-// const priceRangeFilter = document.getElementById('price_range_filter');
-
-// filterCheckboxes.forEach((checkbox) => {
-//     checkbox.addEventListener('change', filterAndDisplay);
-// });
-
-// priceRangeFilter.addEventListener('input', filterAndDisplay);
-
-
-
 const productsContainer = document.getElementById("products_container");
 const sortingSelect = document.getElementById("sorting");
 
 // Flag to track whether sorting is applied
 let isSortingApplied = false;
-let mockData = [];
 
-// Function to display the data
-function displayData(data) {
-  // Clear the existing products container
-  productsContainer.innerHTML = '';
-
-  data.forEach((product) => {
-    const productContainer = document.createElement("div");
-    productContainer.className = "product";
-
-    // Assuming you want to display the first image in the images array
-    const productImage = document.createElement("img");
-    productImage.src = product.images[0];
-    productImage.alt = product.name;
-    productImage.classList.add("product_img_container");
-
-    const productName = document.createElement("h2");
-    productName.textContent = product.name;
-
-    const productPrice = document.createElement("p");
-    productPrice.textContent = `$${product.price.toFixed(2)}`;
-
-    productContainer.appendChild(productImage);
-    productContainer.appendChild(productName);
-    productContainer.appendChild(productPrice);
-
-    productsContainer.appendChild(productContainer);
-  });
-}
-
-// Function to fetch data from the API and update mockData
 function fetchDataFromAPI() {
-  fetch('https://drab-gold-goat-sock.cyclic.app/tyres') // Update the API URL
+  fetch('https://drab-gold-goat-sock.cyclic.app/tyres')
     .then(response => response.json())
     .then(data => {
-      mockData = data; // Update mockData with the fetched data
-      displayData(mockData); // Display the updated data
-      sortAndDisplay(sortingSelect.value); // Reapply sorting
+      popularkData = data;
+      populateProducts();
+      displayData(popularkData);
+      sortAndDisplay(sortingSelect.value);
     })
     .catch(error => {
       console.error('Error fetching data from the API', error);
@@ -252,7 +20,24 @@ function fetchDataFromAPI() {
 }
 
 // Call the function to fetch data from the API when the page loads
-// fetchDataFromAPI();
+fetchDataFromAPI();
+
+// Function to display the data
+function displayData(data) {
+  // Clear the existing products container
+  productsContainer.innerHTML = '';
+
+  data.forEach((product) => {
+    const productContainer = createProductElement(product);
+    productsContainer.appendChild(productContainer);
+
+  });
+}
+
+let popularkData = [];
+const productsPerPage = 4;
+let currentStartIndex = 0;
+let currentPage = 1;
 
 // Sorting
 sortingSelect.addEventListener("change", () => {
@@ -267,16 +52,16 @@ function sortAndDisplay(selectedOption) {
 
     switch (selectedOption) {
       case "atoz":
-        mockData.sort((a, b) => a.name.localeCompare(b.name));
+        popularkData.sort((a, b) => a.name.localeCompare(b.name));
         break;
       case "ztoa":
-        mockData.sort((a, b) => b.name.localeCompare(a.name));
+        popularkData.sort((a, b) => b.name.localeCompare(a.name));
         break;
       case "lowtohigh":
-        mockData.sort((a, b) => a.price - b.price);
+        popularkData.sort((a, b) => a.price - b.price);
         break;
       case "hightolow":
-        mockData.sort((a, b) => b.price - a.price);
+        popularkData.sort((a, b) => b.price - a.price);
         break;
       default:
         // If no valid sorting option is selected, reset the flag
@@ -286,16 +71,8 @@ function sortAndDisplay(selectedOption) {
   }
 
   // Display the sorted or unsorted data
-  displayData(mockData);
+  displayCurrentPage();
 }
-
-
-// filter part 
-
-// Define an array to store the fetched API data
-// let mockData = [];
-
-
 
 // Function to filter and display the data based on selected filters
 function filterAndDisplay() {
@@ -315,6 +92,7 @@ function filterAndDisplay() {
   brandCheckboxes.forEach(checkbox => {
     if (checkbox.checked) {
       selectedBrands.push(checkbox.value);
+      
     }
   });
 
@@ -323,7 +101,7 @@ function filterAndDisplay() {
   const selectedPrice = priceRangeFilter.value;
 
   // Filter products based on selected filters
-  const filteredData = mockData.filter(product => {
+  const filteredData = popularkData.filter(product => {
     const categoryMatch = selectedFilters.length === 0 || selectedFilters.includes(product.category);
     const brandMatch = selectedBrands.length === 0 || selectedBrands.includes(product.brand);
     const priceMatch = product.price <= selectedPrice;
@@ -349,3 +127,139 @@ priceRangeFilter.addEventListener('input', filterAndDisplay);
 // Call the function to fetch data from the API when the page loads
 fetchDataFromAPI();
 
+
+
+
+// const popularproductsContainer = document.querySelector(".products-container");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+let currentSlide = 0; // Current slide index
+
+function createProductElement(product) {
+  // Create the product element (e.g., a div containing product information)
+  const productElement = document.createElement('div');
+  productElement.classList.add('product');
+
+  const h3 = document.createElement("h3");
+  h3.textContent = product.name;
+
+  const price = document.createElement("p");
+  price.textContent = `Price:  $${product.price}`;
+
+  const img = document.createElement("img");
+  img.setAttribute("src", product.images[0]);
+  img.setAttribute("alt", product.name);
+
+  const heartButton = document.createElement("button");
+  heartButton.innerHTML = "&#9825;" // This is the heart symbol
+  // Add a click event listener to handle adding the product to local storage
+  heartButton.addEventListener("click", () => {
+
+    heartButton.innerHTML = "❤️"
+    heartButton.style.Color = ("red");
+    localStorage.setItem(`product_${product.id}`, JSON.stringify(product));
+  });
+
+  productElement.addEventListener("click", function () {
+    window.location.href = "./item.html"
+    localStorage.setItem('productkey', JSON.stringify(product));
+  })
+
+  productElement.appendChild(img);
+  productElement.appendChild(h3);
+  productElement.appendChild(price);
+  productElement.appendChild(heartButton);
+
+  return productElement;
+}
+
+
+
+
+// Populate products into the carousel (You can use your data source)
+
+
+// Populate products into the carousel (You can use your data source)
+function populateProducts() {
+  // Clear existing products
+  const productsContainer = document.querySelector(".products-container");
+  productsContainer.innerHTML = "";
+
+  // Display products based on the currentSlide
+  for (let i = currentSlide; i < currentSlide + productsPerPage; i++) {
+    if (i < popularkData.length) {
+      const product = popularkData[i];
+      const productElement = createProductElement(product);
+      productsContainer.appendChild(productElement); // Use productsContainer here
+    }
+  }
+}
+
+
+// Event listeners for navigation
+prevButton.addEventListener("click", () => {
+  if (currentSlide > 0) {
+    currentSlide -= productsPerPage;
+    populateProducts();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentSlide + productsPerPage < popularkData.length) {
+    currentSlide += productsPerPage;
+    populateProducts();
+  }
+});
+
+// Initial population of products
+populateProducts();
+
+
+//pagination
+
+const productsPerPageList = 11;
+function displayProducts(pageNumber, productsPerPage) {
+  // Calculate the start and end indices for the current page
+  const startIndex = (pageNumber - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+
+  // Clear the existing products container
+  const productsContainer = document.getElementById("products_container");
+  productsContainer.innerHTML = '';
+
+  // Display products for the current page
+  for (let i = startIndex; i < endIndex && i < popularkData.length; i++) {
+    const product = popularkData[i];
+    const productContainer = createProductElement(product);
+    productsContainer.appendChild(productContainer);
+  }
+}
+
+
+function createPaginationButtons() {
+  const totalPages = Math.ceil(popularkData.length / productsPerPageList);
+
+  paginationContainer.innerHTML = '';
+
+  for (let i = 1; i <= totalPages; i++) {
+    const pageButton = document.createElement("button");
+    pageButton.textContent = i;
+    pageButton.addEventListener("click", () => {
+      currentPage = i;
+      displayCurrentPage();
+    });
+    paginationContainer.appendChild(pageButton);
+  }
+}
+
+function displayCurrentPage() {
+  const startIndex = (currentPage - 1) * productsPerPageList;
+  const endIndex = startIndex + productsPerPageList;
+  const productsToDisplay = popularkData.slice(startIndex, endIndex);
+
+  displayData(productsToDisplay);
+}
+
+// Initial population of the first page
+displayCurrentPage();
